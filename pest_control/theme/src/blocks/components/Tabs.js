daeb84ddc51b45248482c0205c3e33857eb910d6
Filipp Zhuravlev <shiningfinger@list.ru>
2017-09-07 15:02:46 +0300

    I've builded a single service section.

--- pest_control/theme/src/blocks/components/Tabs.js
+++ pest_control/theme/src/blocks/components/Tabs.js
@@ -15,6 +15,12 @@ const Tabs = ({
       [`${block}--active`]: isActive
     })
   );
+  const getClasses = (block, modifier) => (
+    classNames({
+      [block]: true,
+      [`${block}--${modifier}`]: !!modifier
+    })
+  );
 
   return (
     <div className='serviceTabs'>
@@ -35,18 +41,18 @@ const Tabs = ({
       <table className='serviceTabs__table'>
         <thead >
           <tr className='serviceTabsTableHeader'>
-            <th className='serviceTabsTableHeader__cell'>Площадь</th>
-            <th className='serviceTabsTableHeader__cell'>Цена</th>
-            <th className='serviceTabsTableHeader__cell'>Гарантия</th>
+            <th className={getClasses('serviceTabsTableHeader__cell', 'name_space')}>Площадь</th>
+            <th className={getClasses('serviceTabsTableHeader__cell', 'name_price')}>Цена</th>
+            <th className={getClasses('serviceTabsTableHeader__cell', 'name_ensure')}>Гарантия</th>
           </tr>
         </thead>
         <tbody>
           {spaces.map(space=> (
             <tr key={space.id}
               className='serviceTabsTableData'>
-              <th className='serviceTabsTableData__cell'>{space.space_name}</th>
-              <th className='serviceTabsTableData__cell serviceTabsTableData__cell--name_price'>{`${space.price}₽`}</th>
-              <th className='serviceTabsTableData__cell'>{space.ensure}</th>
+              <th className={getClasses('serviceTabsTableData__cell', 'name_space')}>{space.space_name}</th>
+              <th className={getClasses('serviceTabsTableData__cell', 'name_price')}>{`${space.price}₽`}</th>
+              <th className={getClasses('serviceTabsTableData__cell', 'name_ensure')}>{space.ensure}</th>
             </tr>
           ))}
         </tbody>
